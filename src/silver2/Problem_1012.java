9f54b28a8ea07d477a21a02bda6b726d73cb386e
hayarobys <hayarobys@gmail.com>
2020-12-24 14:31:43 +0900

    풀이추가

--- src/silver2/Problem_1012.java
+++ src/silver2/Problem_1012.java
@@ -1,20 +1,7 @@
 package silver2;
 
 import java.util.Scanner;
-/*
-1
-6 6 10
-0 1
-1 0
-1 1
-1 2
-2 1
-3 4
-4 4
-4 3
-4 5
-5 4
- */
+
 // 유기농 배추 https://www.acmicpc.net/problem/1012
 class Problem_1012 {
 	public static void main(String[] args) {
@@ -36,24 +23,39 @@ class Problem_1012 {
 	}
 	
 	public static void analyze(final int[][] field) {
+		int[][] statusField = new int[field.length][field[0].length];
 		int worm = 0;
 		for(int x=0; x<field.length; x++) {
 			for(int y=0; y<field[x].length; y++) {
-				System.out.println("\n("+x+","+y+") 값: " + field[x][y] + " " + (field[x][y]==1));
-				if(field[x][y]==1 && isIsolated(field, x, y)) {
+				if(field[x][y]==1 && isIsolated(field, statusField, x, y)) {
 					worm++;
-					System.out.println("\n("+x+","+y+") 고립: " + worm);
+					/*
+					System.out.println("\t+1");
+					for(int b=0; b<statusField[0].length; b++) {
+						for(int a=0; a<statusField.length; a++) {
+							System.out.print(statusField[a][b] + "  ");
+						}
+						System.out.println();
+					}
+					System.out.println();
+					*/
 				}
 			}
 		}
 		System.out.println(worm);
 	}
 	
-	public static boolean isIsolated(final int[][] field, int x, int y) {
-		if(y>0 && field[x][y-1]!=0) {System.out.print("상"); return false;} // 상
-		if(y<field[x].length-1 && field[x][y+1]!=0) {System.out.print("하"); return false;} // 하
-		if(x>0 && field[x-1][y]!=0) {System.out.print("좌"); return false;} // 좌
-		if(x<field.length-1 && field[x+1][y]!=0) {System.out.print("우"); return false;} // 우
+	public static boolean isIsolated(final int[][] field, int[][] statusField, int x, int y) {
+		// dfs 알고리즘(깊이 우선 탐색)
+		if(statusField[x][y] != 0){return false;} // 이미 방문한 노드
+		//System.out.print(new StringBuilder().append("(").append(x).append(", ").append(y).append(")"));
+		
+		statusField[x][y]=1;
+		if(y>0 && field[x][y-1]==1){ isIsolated(field, statusField, x, y-1); } // 상
+		if(y<statusField[x].length-1 && field[x][y+1]==1){ isIsolated(field, statusField, x, y+1); } // 하
+		if(x>0 && field[x-1][y]==1){ isIsolated(field, statusField, x-1, y); } // 좌
+		if(x<statusField.length-1 && field[x+1][y]==1){ isIsolated(field, statusField, x+1, y); } // 우
+		
 		return true;
 	}
 }
